1f55a3ccaeac3221ac8dbba310b6daaf2e55ddf5
DimasWaldy <dimaswaldy@gmail.com>
2025-07-22 19:52:38 +0700

    Initial Commit perulangan

---
+++ Python_Pengulangan.py
@@ -0,0 +1,14 @@
+"""
+Program membaca buku / perulangan
+"""
+
+jumlah_buku = 10
+print('Ibu berkata, "Baca semua buku mu"')
+
+jumlah_yang_sudah_dibaca = 0
+print(f"Jumlah buku yang sudah dibaca {jumlah_yang_sudah_dibaca} sudah dibaca")
+
+for jumlah_yang_sudah_dibaca in range(1, 11):
+    print(f"Buku ke {jumlah_yang_sudah_dibaca} sudah dibaca")
+
+print(f"Jumlah buku yang sudah dibaca {jumlah_yang_sudah_dibaca}")
